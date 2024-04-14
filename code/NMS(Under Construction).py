c77b296867b4b04a2080d8a5d2831675c2d6e5d6
cyanblaze <evanhu168@gmail.com>
2024-04-14 17:57:14 +0800

    NMS update augment

--- code/NMS(Under Construction).py
+++ code/NMS(Under Construction).py
@@ -1,9 +1,10 @@
 from Harris_by_ShuoEn import *
 import numpy as np
 
-def NMS_Harris(image, Point_limit:int, k=0.04, thresRatio=0.01):
+def NMS_Harris(image, Point_limit:int, MinDis:float, k=0.04, Harris_thresRatio=0.01):
     #Point_limit(N): the function will stop after finding N points using NMS
-    #thresRaito: Used for determine the Threshold for NMS that knows which feature points to delete
+    #MinDis: Used for determine the Threshold for NMS that knows which feature points to delete
+    #Harris_thresRatio=0.01: A variable used for Harris-part's unwanted points' deletion 
 
     # Compute x and y derivatives of image
     Ix, Iy = compute_gradient(image)
@@ -28,10 +29,10 @@ def NMS_Harris(image, Point_limit:int, k=0.04, thresRatio=0.01):
     # Threshold on value of R and local maximum
     #  Also used for NMS's deletion, which is, when the points are starting to get smaller than the threshold,
     # the function return and exit
-    threshold = thresRatio*np.max(R)
+    threshold = Harris_thresRatio*np.max(R)
     R[R<threshold] = 0
     localMaxR = maximum_filter(R, size=3, mode='constant')
-    R[R<localMaxR] = 0
+    R[R<localMaxR] = 0 
 
     #return point
     # #point is a list of Feature Points
